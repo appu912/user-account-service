0dcefb5682af5cf4cf89623fc0570f40102f8a5f
Apoorv Mehra <mehra.appu912@gmail.com>
2026-01-26 19:29:06 +0000

    Updated dependencies versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "user-account-service"
+
+pluginManagement {
+    val springBootVersion: String by settings
+    val spotlessVersion: String by settings
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("com.diffplug.spotless") version spotlessVersion
+    }
+}
+
